10541eedeefba4fe03a4bc9987cf3f3a370c7fa9
Vortexx2 <janmayshuklay@gmail.com>
2023-07-09 05:13:40 +0530

    Add search route

--- backend/controllers/Video.js
+++ backend/controllers/Video.js
@@ -1,21 +1,79 @@
-const Video=require('../models/Video');
+const Video = require('../models/Video');
 
-exports.createVideo=async(req,res)=>{
-    try{
-        const {title,description,author,views,creator,url,length,tags,garde,subject,lang,chapter}=req.body;
-        console.log(title,description,author,views,creator,url,length,tags,garde,subject,lang,chapter);
-        const video=await Video.create({title,description,author,views,creator,url,length,tags,garde,subject,lang,chapter});
-        res.status(200).json({
-            success:true,
-            message:"video created",
-            video,
-        })
-    }catch(error){
-        res
-        .status(500)
-        .json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
+exports.createVideo = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      author,
+      views,
+      creator,
+      url,
+      length,
+      tags,
+      garde,
+      subject,
+      lang,
+      chapter,
+    } = req.body;
+    console.log(
+      title,
+      description,
+      author,
+      views,
+      creator,
+      url,
+      length,
+      tags,
+      garde,
+      subject,
+      lang,
+      chapter
+    );
+    const video = await Video.create({
+      title,
+      description,
+      author,
+      views,
+      creator,
+      url,
+      length,
+      tags,
+      garde,
+      subject,
+      lang,
+      chapter,
+    });
+    res.status(200).json({
+      success: true,
+      message: 'video created',
+      video,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.searchVideo = async (req, res) => {
+  const search = req.body.text;
+
+  //   if (!search)
+  //     return res.status(401).json({ success: false, message: 'Text is missing' });
+
+  const videos = await Video.find();
+  if (!search) {
+    return res.status(300).json({
+      success: true,
+      payload: videos,
+    });
+  }
+  const filtered_videos = videos.filter(vid => vid.title.toLowerCase().includes(search));
+
+  res.status(300).json({
+    success: true,
+    payload: filtered_videos,
+  });
+};
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,19 +1,26 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const {studentSignUp,studentLogIn,creatorSignUp,creatorLogIn,modulatorSignUp,modulatorLogIn} = require('../controllers/Auth');
-const {auth,isStudent,isCreator} = require('../middlewares/auth');
-const {createScholarship}=require('../controllers/Scholarship');
-const {createVideo}=require('../controllers/Video');
+const {
+  studentSignUp,
+  studentLogIn,
+  creatorSignUp,
+  creatorLogIn,
+  modulatorSignUp,
+  modulatorLogIn,
+} = require('../controllers/Auth');
+const { auth, isStudent, isCreator } = require('../middlewares/auth');
+const { createScholarship } = require('../controllers/Scholarship');
+const { createVideo, searchVideo } = require('../controllers/Video');
 
-router.post("/student/signup",studentSignUp);
-router.post('/student/login',studentLogIn);
-router.post("/creator/signup",creatorSignUp);
-router.post('/creator/login',creatorLogIn);
-router.post("/modulator/signup",modulatorSignUp);
-router.post('/modulator/login',modulatorLogIn);
-router.post('/scholarship/create',createScholarship);
-router.post('/creater/video/create',createVideo);
+router.post('/student/signup', studentSignUp);
+router.post('/student/login', studentLogIn);
+router.post('/creator/signup', creatorSignUp);
+router.post('/creator/login', creatorLogIn);
+router.post('/modulator/signup', modulatorSignUp);
+router.post('/modulator/login', modulatorLogIn);
+router.post('/scholarship/create', createScholarship);
+router.post('/creater/video/create', createVideo);
+router.get('/videos/search', searchVideo);
 
-
-module.exports=router;
+module.exports = router;
